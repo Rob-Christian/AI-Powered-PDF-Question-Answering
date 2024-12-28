5cb6c62e22d0cf95d9fa59485302d5392dac519e
Rob Christian Caduyac <59250293+Rob-Christian@users.noreply.github.com>
2024-12-28 17:09:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import streamlit as st
 import langchain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstore import Chroma
+from langchain.vectorstores import Chroma
 from langchain import OpenAI, RetrievalQA
