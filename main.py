4c6f41ac04729b5a39dbca425a15ce22c6d9ea0e
Rob Christian Caduyac <59250293+Rob-Christian@users.noreply.github.com>
2024-12-28 17:05:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Import necessary libraries
 import streamlit as st
 import langchain
-from langchain.text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.openai.embeddings import OpenAIEmbeddings
 from langchain.openai.chat_models import ChatOpenAI
